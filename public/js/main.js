58a6009abf81e01e8b023d594f8b5e643e7a8036
happybirthdaybindu <happybirthdaybindu@gmail.com>
2020-04-26 04:24:19 +0530

    Let's do dis again

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:7,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:false,
+                animateOut: 'fadeOut',
+
+                loop:false
+            }
+        }
+    });
+
+
+
+});
